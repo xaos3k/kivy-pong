3f9cc173bd67adade452dc5495cc127a79a20ad5
xaos3k <xaos3k@gmail.com>
2019-06-14 22:18:07 +0200

    vel = bounced * 1.2

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class PongPaddle(Widget):
             vx, vy = ball.velocity
             offset = (ball.center_y - self.center_y) / (self.height / 2)
             bounced = Vector(-1 * vx, vy)
-            vel = bounced * 1.1
+            vel = bounced * 1.2
             ball.velocity = vel.x, vel.y + offset
 
 
